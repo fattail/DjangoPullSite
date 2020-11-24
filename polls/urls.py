4d05a747ab5c6533ed5cc574a3d0091af2c8eb11
HitMe69 <lorry911@hotmail.com>
2020-11-24 14:16:31 +0800

    add comma

--- polls/urls.py
+++ polls/urls.py
@@ -1,3 +1,11 @@
+'''
+Author: your name
+Date: 2020-11-23 08:45:20
+LastEditTime: 2020-11-23 13:49:21
+LastEditors: Please set LastEditors
+Description: In User Settings Edit
+FilePath: \Pull_Site\polls\urls.py
+'''
 from django.urls import path
 
 from . import views
@@ -5,5 +13,5 @@ from . import views
 urlpatterns = [
     path('',views.index,name='index'),
     path('<int:question_id>/',views.detail,name='detail'),
-    path('<int:question_id>')
+    path('<int:question_id>'),
 ]

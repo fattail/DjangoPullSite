e6c8deeffbe5ae24a395b44a4d471018e59e6d86
HitMe69 <lorry911@hotmail.com>
2020-12-02 23:10:58 +0800

    edit

--- polls/views.py
+++ polls/views.py
@@ -11,6 +11,7 @@ from django.template import loader
 from django.urls import reverse
 from django.http import HttpResponseRedirect
 from django.views import generic
+from django.utils import timezone
 
 from .models import Question,Choice
 # Create your views here.
@@ -21,7 +22,7 @@ class IndexView(generic.ListView):
     
     def get_queryset(self):
         """return the last 5 published questions"""
-        return Question.objects.order_by('-pub_date')[:5]
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
 
 class DetailView(generic.DetailView):
     model = Question
@@ -30,19 +31,6 @@ class DetailView(generic.DetailView):
 class ResultsView(generic.DetailView):
     model = Question
     template_name = 'polls/results.html'
-
-def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    context = {'latest_question_list': latest_question_list}
-    return render(request, 'polls/index.html', context)
-
-def detail(request,question_id):
-    question = get_object_or_404(Question,pk=question_id)
-    return render(request,'polls/detail.html',{'question':question})
-
-def results(request,question_id):
-    question = get_object_or_404(Question,pk=question_id)
-    return render(request,'polls/results.html',{'question':question})
 
 def vote(request,question_id):
     question = get_object_or_404(Question,pk=question_id)

a3dd933ee809f7ac4bce22a538be452b2cf6438d
HitMe69 <lorry911@hotmail.com>
2020-12-03 23:23:12 +0800

    edit

--- polls/admin.py
+++ polls/admin.py
@@ -3,7 +3,18 @@ from django.contrib import admin
 from .models import Question,Choice
 
 # Register your models here.
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+    
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ('问题正文内容',{'fields':['question_text']}),
+        ('发布日期信息',{'fields':['pub_date']}),
+    ]
+    inlines = [ChoiceInline]
+    list_display = ('question_text','pub_date')
+    list_filter = ['pub_date']
+    search_fields = ['question_text']
 
-admin.site.register(Question)
-
-admin.site.register(Choice)
+admin.site.register(Question,QuestionAdmin)
